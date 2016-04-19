a3859ecc5ed7c52c8308bd2a8a62fbc422527c72
rjbhewei <rjbhewei@gmail.com>
2016-04-19 15:45:18 +0800

    1111

--- src/main/java/com/c1000k/netty/client/Netty5C1000kClient.java
+++ src/main/java/com/c1000k/netty/client/Netty5C1000kClient.java
@@ -23,26 +23,28 @@ import java.util.Set;
  */
 public class Netty5C1000kClient {
 
-    static final String URL = System.getProperty("url", "ws://127.0.0.1:8080/websocket");
-
-    //public static final ExecutorService EXECUTOR_SERVICE= Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
-
     public static void main(String[] args) throws Exception {
-        Set<ChannelFuture> futures=new HashSet<>();
+        if(args == null || args.length != 4) {
+            return;
+        }
+        String localIp = args[0];
+        int count = Integer.parseInt(args[1]);
+        String wsIp = args[2];
+        int port = Integer.parseInt(args[3]);
+        Set<ChannelFuture> futures = new HashSet<>();
+        String URL = System.getProperty("url", "ws://" + wsIp + ":" + port + "/websocket");  //System.getProperty("url", "ws://127.0.0.1:8080/websocket");
+        URI uri = new URI(URL);
         EventLoopGroup group = new NioEventLoopGroup();
-        for(int i = 0; i < 5; i++) {
-            futures.add(new Netty5C1000kClient().start("192.168.18.101", group));
+        for(int i = 0; i < count; i++) {
+            futures.add(new Netty5C1000kClient().start(localIp, group, uri));
         }
-
-        for(ChannelFuture future:futures){
-             future.sync();
+        for(ChannelFuture future : futures) {
+            future.sync();
         }
-
         group.shutdownGracefully();
     }
 
-    public ChannelFuture start(String ip, EventLoopGroup group) throws Exception {
-        URI uri = new URI(URL);
+    public ChannelFuture start(String ip, EventLoopGroup group, URI uri) throws Exception {
         final WebSocketClientHandler handler = new WebSocketClientHandler(WebSocketClientHandshakerFactory.newHandshaker(uri, WebSocketVersion.V13, null, false, new DefaultHttpHeaders()));
         Bootstrap b = new Bootstrap();
         b.group(group).channel(NioSocketChannel.class).handler(new ChannelInitializer<SocketChannel>() {
